253cf67d4e00808359d74354d4655d20630354eb
Ulf-vom-Mond <noahrullmann@gmail.com>
2023-11-04 20:20:15 +0100

    basic game principles are working

---
+++ hello_world.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var keys = 0;
+
+const { SerialPort } = require('serialport')
+
+const serial = new SerialPort({
+  path: '/dev/ttyUSB0',
+  baudRate: 115200,
+})
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '.')));
+
+app.listen(port, function(){
+   console.log('Listening on port ' + port)
+})
+
+app.get('/keys', function (req, res) {
+    res.json({
+      keys: keys
+    })
+});
+
+
+serial.on('data', function (data) {
+  keys = data[data.length - 1]
+})
+
+serial.on('error', function(err) {
+  console.log('Error: ', err.message)
+})
